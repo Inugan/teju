fb658af82071ed62b985f2517e3223b6915c900b
root <root@ip-172-31-35-248.ap-south-1.compute.internal>
2020-08-06 15:16:54 +0000

    git.sh added

---
+++ git.sh
@@ -0,0 +1,11 @@
+git push origin :IB
+git push origin :br_stud
+git push origin :inr
+git push origin :int
+git push origin :s1
+git push origin :t1git push origin :IB
+git push origin :br_stud
+git push origin :inr
+git push origin :int
+git push origin :s1
+git push origin :t1
